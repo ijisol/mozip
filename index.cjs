b518e41d589bcc99045127af37e562f2033cd2c0
ijisol <ijisol@naver.com>
2024-10-30 16:05:55 +0900

    Support CJS module

---
+++ index.cjs
@@ -0,0 +1,11 @@
+'use strict';
+
+const { Buffer } = require('node:buffer');
+const { normalize } = require('node:path/posix');
+const { Transform } = require('node:stream');
+const { promisify } = require('node:util');
+const { crc32, deflateRaw } = require('node:zlib');
+
+// Paste code...
+
+module.exports = { ZipStream, dateToDosDateTime };
